464bec7e65ba0d95a1150456604c4fcaf9133135
Joanna Derks <joanna.derks@schibsted.pl>
2019-10-07 18:19:04 +0200

    Order results by date

--- modules/food-time/resolvers/stats.js
+++ modules/food-time/resolvers/stats.js
@@ -2,13 +2,18 @@ const knex = require('../../../knex')
 
 module.exports = async (_, {spaceId}, context) => {
   const results = await knex
-    .sum('extra_food as extra_food')
-    .select(knex.raw('strftime(\'%d/%m/%Y \', datetime(time, \'unixepoch\')) as date'))
-    .from('entries')
-    .where({spaceId})
-    .groupBy('date')
-    .orderBy('time', 'desc')
-    .limit(30)
+    .select('*')
+    .from(function() {
+      this.sum('extra_food as extra_food')
+        .select(knex.raw('strftime(\'%d/%m/%Y \', datetime(time, \'unixepoch\')) as date'), 'time')
+        .from('entries')
+        .where({spaceId})
+        .groupBy('date')
+        .orderBy('time', 'desc')
+        .limit(30)
+    })
+    .orderBy('time')
+
 
   return {
     extra_food_per_day: results
